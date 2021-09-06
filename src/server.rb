7f35842ff089eddc950fc02426c5e8547788a98b
dayvidwhy <dayvidwhy@gmail.com>
2021-09-06 22:37:02 +1000

    Threads exit on shutdown.

--- src/server.rb
+++ src/server.rb
@@ -5,54 +5,58 @@ require 'json'
 # load our radar
 require_relative './radar.rb'
 
-# Main Sinatra class instantiates the server
+# Helper to setup our routes on the server and then
+# provides a method to actually start the server.
 class Server < Sinatra::Base
-    # configure sinatra
-    Server.configure do
+    set :radars, Array.new
+
+    helpers do
+        def radars; self.class.radars; end
+    end
+
+    configure do
         # sinatra traps system interupts
         # turning this off helps us stop the server
         # if one of our checking loops is still running
         disable :traps
     end
-end
 
-# Helper to setup our routes on the server and then
-# provides a method to actually start the server.
-class ServerHandler
-    def initialize(server)
-        @server = server
-        @server.get "/" do
-            "app is running"
-        end
-        
-        # simple endpoint that receives a url and request type to check
-        @server.post "/check" do
-            check = JSON.parse(request.body.read)
-        
-            # check loop in new thread per request
-            Thread.new {
-                responseRadar = ResponseRadar.new(check["url"], check["hook"])
-                responseRadar.beginChecking
-            }
-        
-            # let the client know we have their request
-            res = Hash.new
-            res["status"] = "Received request to keep check."
-            body (JSON.generate(res))
-            status 200
-        end
-        
-        # hook to capture a notification when an address goes down
-        @server.get "/notify" do
-            puts "Was notified of being down"
-            status 200
-        end
+    get "/" do
+        "app is running"
     end
 
-    def start
-        @server.run!
+    # simple endpoint that receives a url and request type to check
+    post "/check" do
+        check = JSON.parse(request.body.read)
+    
+        # check loop in new thread per request
+        radars << Thread.new {
+            responseRadar = ResponseRadar.new(check["url"], check["hook"])
+            responseRadar.beginChecking
+        }
+    
+        # let the client know we have their request
+        res = Hash.new
+        res["status"] = "Received request to keep check."
+        body (JSON.generate(res))
+        status 200
+    end
+    
+    # hook to capture a notification when an address goes down
+    get "/notify" do
+        puts "Was notified of being down"
+        status 200
+    end
+
+    begin
+        run!
+    rescue Interrupt => e
+        quit!
+        radars.each do |radar|
+            radar.exit
+        end
+        STDERR.puts "Process interrupted, shutting down."
+    rescue
+        STDERR.puts "Other exception."
     end
 end
-
-server = ServerHandler.new(Server)
-server.start
